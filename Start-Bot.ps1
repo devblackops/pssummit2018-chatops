1c8495caffcf2c842c36e6b9baedb12e02c2da35
Brandon Olin <brandon@devblackops.io>
2018-04-11 22:21:28 -0700

    Load some config stuff from environment variables

--- Start-Bot.ps1
+++ Start-Bot.ps1
@@ -1,15 +1,27 @@
 [cmdletbinding()]
 param()
 
-Import-Module PoshBot -Force -Verbose:$false
+Import-Module Poshbot -Force -Verbose:$false
 
-if ($IsWindows) {
-    $poshBotDir = Join-Path -Path $env:USERPROFILE -ChildPath '.poshbot'
-} else {
-    $poshBotDir = Join-Path -Path $env:HOME -ChildPath '.poshbot'
-}
-$configFile = (Join-Path -Path $poshBotDir -ChildPath 'Cherry2000.psd1')
+# Get config
+$poshBotDir = Join-Path -Path $env:HOME -ChildPath '.poshbot'
+$configFile = Join-Path -Path $poshBotDir -ChildPath 'Cherry2000.psd1'
 $config = Get-PoshBotConfiguration $configFile
+
+# Add plugin config
+$config.PluginConfiguration = @{
+    'PoshBot.PSSummit' = @{
+        GoogleApiShortenerApiKey = $env:GOOGLE_API_SHORTENER_API_KEY
+    }
+    'PoshBot.Wolfram' = @{
+        ApiKey = $env:WOLFRAM_API_KEY
+    }
+}
+
+# Create chat backend
+$config.BackendConfiguration.Token = $env:POSHBOT_SLACK_API_KEY
 $backend = New-PoshBotSlackBackend -Configuration $config.BackendConfiguration
+
+# Create and start bot
 $bot = New-PoshBotInstance -Backend $backend -Configuration $config -WarningAction SilentlyContinue
 $bot | Start-PoshBot -WarningAction SilentlyContinue
